854aff45f8c0536b42615ddb688c6f3c36ab417a
cristianborcos <38328969+cristianborcos@users.noreply.github.com>
2018-04-25 18:36:44 +0300

    Add files via upload

---
+++ Laptop/Laptop/Laptop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laptop
+{
+    class Laptop
+    {
+        public int screenSize = 14;
+        public int ramMemoryGB = 16;
+        public int processorFrequency = 3;
+        public int hddCapacityGB = 500;
+        public string color = "BLUE";
+        public string keyboardLayout = "US";
+
+        public void ShowAttributes()
+
+        {
+            Console.WriteLine($"ScreenSize:{screenSize}, RamMemoryGB:{ramMemoryGB}, processorFrequency:{processorFrequency}, HDDCapacity:{hddCapacityGB}, Color:{color}, KeyboardLayout:{keyboardLayout}");
+        }
+
+        public int ModifyprocessorFrequency(int newprocessorFrequency)
+        {
+            processorFrequency = processorFrequency + newprocessorFrequency;
+
+            return processorFrequency;
+        }
+
+        public int ModifyRamMemoryGB(int newRamMemoryGB)
+        {
+            ramMemoryGB = ramMemoryGB + newRamMemoryGB;
+
+            return ramMemoryGB;
+        }
+        public int ModifyscreenSize(int newscreenSize)
+        {
+            screenSize = screenSize + newscreenSize;
+
+            return screenSize;
+        }
+       
+       public int ModifiyhddCapacityGB(int newhddCapacityGB)
+        {
+            hddCapacityGB = hddCapacityGB + newhddCapacityGB;
+
+            return hddCapacityGB;
+
+        }
+
+       
+        
+        
+
+        
+    }
+}
---
+++ Laptop/Laptop/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Laptop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop Laptop = new Laptop();
+            Laptop.ShowAttributes();
+            Laptop.color = "BLUE";
+            Laptop.ShowAttributes();
+            Console.WriteLine(Laptop.ModifyRamMemoryGB(16));
+            Console.WriteLine(Laptop.ModifyprocessorFrequency(2));
+            Console.WriteLine(Laptop.ModifyscreenSize(1));
+            Console.WriteLine(Laptop.ModifiyhddCapacityGB(600));
+            int result3 = Laptop.ModifyprocessorFrequency(2) + Laptop.ModifyprocessorFrequency(2);
+            int result = Laptop.ModifyRamMemoryGB(16) + Laptop.ModifyRamMemoryGB(16);
+            int result4 = Laptop.ModifyscreenSize(1) + Laptop.ModifyscreenSize(1);
+            int result5 = Laptop.ModifiyhddCapacityGB(500) + Laptop.ModifiyhddCapacityGB(100);
+            
+            
+
+
+            Laptop.ShowAttributes();
+
+            Console.WriteLine(result);
+
+            Console.WriteLine(Add(20, 2));
+
+            int result2 = Add(16, 18);
+            
+            
+
+            
+            Console.ReadKey();
+    
+        }
+        static int Add(int x, int y)
+
+        {
+            return x + y;
+
+        }
+    }
+}
